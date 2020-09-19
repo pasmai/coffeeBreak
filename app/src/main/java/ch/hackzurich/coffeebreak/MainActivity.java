669b918f1e2e75145b89f9930b3c43c7dec0b105
Elias Huwyler <elias.huwyler@gmail.com>
2020-09-19 17:33:48 +0200

    hand off to invite activity

--- app/src/main/java/ch/hackzurich/coffeebreak/MainActivity.java
+++ app/src/main/java/ch/hackzurich/coffeebreak/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -44,7 +45,23 @@ public class MainActivity extends AppCompatActivity {
                             new AuthUI.IdpConfig.EmailBuilder().build(),
                             new AuthUI.IdpConfig.PhoneBuilder().build())).build(),
                     RC_SIGN_IN);
+        }else{
+            startActivity(new Intent(MainActivity.this, InviteActivity.class));
         }
         //updateUI(currentUser);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                startActivity(new Intent(MainActivity.this, InviteActivity.class));
+                finish();
+            } else {
+            }
+        }
     }
 }
