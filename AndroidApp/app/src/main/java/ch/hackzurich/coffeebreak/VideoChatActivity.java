b4a5644a4816a03c816e945af91684b2eca7cbd5
pasmai <pascal.maillard@gmx.ch>
2020-09-20 01:38:31 +0200

    fix Jitsi join

--- AndroidApp/app/src/main/java/ch/hackzurich/coffeebreak/VideoChatActivity.java
+++ AndroidApp/app/src/main/java/ch/hackzurich/coffeebreak/VideoChatActivity.java
@@ -10,51 +10,42 @@ import android.widget.Toast;
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+import org.jitsi.meet.sdk.JitsiMeetUserInfo;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-    public class VideoChatActivity extends AppCompatActivity {
+public class VideoChatActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_video_chat);
+    String meetingId;
 
-            // Initialize default options for Jitsi Meet conferences.
-            String meetingId = getIntent().getStringExtra(Config.video_meeting_id);
-            URL serverURL;
-            try {
-                serverURL = new URL("https://meet.jit.si/" + meetingId);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Could not open meeting URL", Toast.LENGTH_SHORT).show();
-                finish();
-                return;
-            }
-            JitsiMeetConferenceOptions defaultOptions
-                    = new JitsiMeetConferenceOptions.Builder()
-                    .setServerURL(serverURL)
-                    .setWelcomePageEnabled(false)
-                    .build();
-            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
 
-        public void onButtonClick(View v) {
-            EditText editText = findViewById(R.id.conferenceName);
-            String text = editText.getText().toString();
-
-            if (text.length() > 0) {
-                // Build options object for joining the conference. The SDK will merge the default
-                // one we set earlier and this one when joining.
-                JitsiMeetConferenceOptions options
-                        = new JitsiMeetConferenceOptions.Builder()
-                        .setRoom(text)
-                        .build();
-                // Launch the new activity with the given options. The launch() method takes care
-                // of creating the required Intent and passing the options.
-                JitsiMeetActivity.launch(this, options);
-            }
-        }
+        meetingId = getIntent().getStringExtra(Config.video_meeting_id);
     }
 
+    public void onButtonClick(View v) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+
+        if (!text.isEmpty()) {
+
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            JitsiMeetUserInfo user = new JitsiMeetUserInfo();
+            user.setDisplayName(text);
+
+            JitsiMeetConferenceOptions options
+                    = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(meetingId)
+                    .setUserInfo(user)
+                    .build();
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+}
